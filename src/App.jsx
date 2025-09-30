07e15cca6cb7c03efd14bea52ecfb11b85252443
Ali <aliazadigithub@gmail.com>
2025-09-30 10:30:22 +0330

    Fix : Change directory assets files

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ import './App.css'
 import Header from './Components/Maincomponents/Header'
 import Main from './Components/Maincomponents/Main'
 import Footer from './Components/Maincomponents/Footer'
+import Login from './Pages/Login'
+import { useRoutes } from 'react-router-dom'
+
+const routes = [
+  {path:'/' , element:<h1>Landing Page</h1>},
+  {path:'/Login' , element:<Login/>},
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = useRoutes(routes)
 
   return (
     <>
-    <Header/>
+    {/* <Header/>
     <Main/>
     <Footer/>
-
+    <Login/> */}
+    {router}
 
     </>
   )
